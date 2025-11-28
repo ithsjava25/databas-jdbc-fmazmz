97456b032f9a52c778a6d397b7c24f9198ea6935
WHITEROSE <firasmoussa60@gmail.com>
2025-11-28 20:33:29 +0100

    Allow deletion of user account

--- src/main/java/com/example/domain/UserService.java
+++ src/main/java/com/example/domain/UserService.java
@@ -2,7 +2,6 @@ package com.example.domain;
 
 import com.example.domain.dto.PasswordUpdateRequest;
 import com.example.domain.dto.UserCreationRequest;
-import com.example.domain.dto.UserDTOMapper;
 import com.example.domain.model.User;
 import com.example.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +9,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-import static org.springframework.util.StringUtils.capitalize;
 
 @Service
 public class UserService {
     private final UserRepository userRepo;
 
-    private final UserDTOMapper mapper;
-
     @Autowired
-    public UserService(UserRepository userRepo, UserDTOMapper mapper) {
+    public UserService(UserRepository userRepo) {
         this.userRepo = userRepo;
-        this.mapper = mapper;
     }
 
     public void createUser(UserCreationRequest req) {
@@ -34,6 +29,15 @@ public class UserService {
 
         user.setName(User.makeUserName(user));
         userRepo.save(user);
+    }
+
+    public void deleteUser(Short userId) {
+        Optional<User> user = userRepo.findById(userId);
+        if (user.isEmpty()) {
+            System.out.println("User not found");
+            return;
+        }
+        userRepo.delete(user.get());
     }
 
     public void updateUserPassword(PasswordUpdateRequest req) {
